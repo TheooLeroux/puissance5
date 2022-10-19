1b3120b7b3e40c2dd49b94809ee5492efc635cee
Mathis95c <114164132+Mathis95c@users.noreply.github.com>
2022-10-19 16:34:55 +0200

    Delete SQL directory

--- SQL/story10.sql
+++
@@ -1,9 +0,0 @@
-SELECT message,name,date_time_Message,id_Sender
-FROM Message 
-INNER JOIN user on message.id_Sender = user.id
-WHERE date_time_Message > (NOW()-interval -1 DAY);
-
-/* 
-Requête permettant de SELECTionner les message compris entre maintenant et maintenant - 24h
-*/
-	
